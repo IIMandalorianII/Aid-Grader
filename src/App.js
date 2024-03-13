06aacbde000a0c1cc061673cb32e8255938f5666
Michael May <67942039+MichaelTheMay@users.noreply.github.com>
2024-03-12 19:28:25 -0500

    Added Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import './App.css';
-import React from 'react'
-import Home from './components/Home'
+import React, { Suspense, lazy } from 'react'
 import UniversityInput from './components/UniversityInput';
 import AidInput from './components/AidInput';
 import Result from './components/Result';
 import { AnimatePresence } from "framer-motion"
 import { Route, Switch, useLocation } from "react-router-dom";
 import { SchoolProvider } from './contexts/SchoolContext'
-import {FormProvider} from './contexts/FormContexts'
+import { FormProvider } from './contexts/FormContexts'
 import Footer from './components/widgets/Footer'
+const Home = lazy(() => import('./components/Home'));
 function App() {
-  const currentRoute = useLocation();
-  return (
-      <SchoolProvider>
-    <FormProvider>
 
-        <div className="App">
+    const currentRoute = useLocation();
+    return (
+        <SchoolProvider>
+            <FormProvider>
 
-          <AnimatePresence exitBeforeEnter>
-            <Switch location={currentRoute} key={currentRoute.key}>
-              <Route path="/" component={Home} exact/>
-              <Route path="/school" component={UniversityInput} exact/>
-              <Route path="/aid" component={AidInput} exact/>
-              <Route path="/results" component={Result} exact/>
+                <div className="App">
 
-            </Switch>
-          </AnimatePresence>
-          <Footer/>
-        </div>
-    </FormProvider>
-      </SchoolProvider>
-  );
+                    <AnimatePresence exitBeforeEnter>
+                        <Suspense fallback={<div>Loading...</div>}>
+
+                            <Switch location={currentRoute} key={currentRoute.key}>
+                                <Route path="/" component={Home} exact />
+                                <Route path="/school" component={UniversityInput} exact />
+                                <Route path="/aid" component={AidInput} exact />
+                                <Route path="/results" component={Result} exact />
+
+                            </Switch>
+                        </Suspense>
+                    </AnimatePresence>
+                    <Footer />
+                </div>
+            </FormProvider>
+        </SchoolProvider>
+    );
 }
 
 export default App;
